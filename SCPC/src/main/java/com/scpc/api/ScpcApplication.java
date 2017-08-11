ffa04362e2359b91582520c6df2d143fa6cce8de
sonsungwon <sw5017@naver.com>
2017-08-11 18:25:13 +0900

    Add files via upload

---
+++ SCPC/src/main/java/com/scpc/api/ScpcApplication.java
@@ -0,0 +1,12 @@
+package com.scpc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScpcApplication.class, args);
+	}
+}
---
+++ SCPC/src/main/java/com/scpc/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.scpc.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ScpcApplication.class);
+	}
+
+}
